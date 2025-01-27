8be08b1d85e0c6f569814eb94da9ee94e536b8f1
David Ordaz <147444473+davidaordaz@users.noreply.github.com>
2025-01-27 16:43:48 +0000

    update

---
+++ Projects/01.Project.py
@@ -0,0 +1,11 @@
+seconds = (float(input("Enter Number of Seconds: ")))
+minutes = seconds/60
+hours = minutes/60
+days = hours/24
+years = days/365
+
+print("Years: " + years)
+print("Days: " + days)
+print("Hours: " + hours)
+print("Minutes: " + minutes)
+print("Seconds: " + seconds)
