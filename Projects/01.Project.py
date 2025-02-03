f45f457caf2d0210fbeaa1808889a8d25d5d5ee6
David Ordaz <147444473+davidaordaz@users.noreply.github.com>
2025-02-03 15:56:16 +0000

    project1

--- Projects/01.Project.py
+++ Projects/01.Project.py
@@ -1,15 +1,20 @@
-
-
 numSeconds = float(input("Enter Number of Seconds: "))
-numMinutes = seconds/60
-numHours = minutes/60
-days = hours/24
-years = days/365
 
+numYears = numSeconds / (60*60*24*365)
+numSeconds %= (60*60*24*365)
 
+numDays = numSeconds / (60*60*24)
+numSeconds %= (60*60*24)
 
-print("Years: ", years)
-print("Days: ", days)
-print("Hours: ", hours)
-print("Minutes: ", minutes)
-print("Seconds: ", seconds)
+numHours = numSeconds / (60*60)
+numSeconds %= (60*60)
+
+numMinutes = numSeconds / 60
+numSeconds %= 60
+
+print("Years:",int(numYears))
+print("Days:",int(numDays))
+print("Hours:",int(numHours))
+print("Minutes:",int(numMinutes))
+print("Seconds:",int(numSeconds))
+
