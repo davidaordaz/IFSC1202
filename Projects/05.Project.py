cae1b020fa73617ac4002e660c423251c10fdc1b
David Ordaz <147444473+davidaordaz@users.noreply.github.com>
2025-02-12 16:17:48 +0000

    project5

--- Projects/05.Project.py
+++ Projects/05.Project.py
@@ -4,14 +4,15 @@ print("Special Numbers Between", num1, "and", num2)
 
 
 for i in range(num1, num2):
-    temp = i
+    temp1 = i
+    temp2 = i
     count = 0
     num3 = 0
-    while num1 > 0:
-        num1 //= 10
+    while temp1 >= 1:
+        temp1 //= 10
         count += 1
-    for j in range(count):
-        temp%=10
-        num3 += temp^count
-    if(num3 == temp):
+    for j in range(0, count):
+        num3 += pow(temp2%10, count)
+        temp2 = int(temp2/10)
+    if(num3 == i):
         print(num3)
