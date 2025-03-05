0c6793739d1c29b9ba00e09647fdacf7fe7a4832
David Ordaz <147444473+davidaordaz@users.noreply.github.com>
2025-03-05 20:18:26 +0000

    project 6

--- Projects/06.Project.py
+++ Projects/06.Project.py
@@ -1,24 +1,28 @@
 outputFile = open("TextFiles/06.Project Output File.txt", "w")
 inputFile = open("TextFiles/06.Project Input File.txt", "r")
 mergeFile = open("TextFiles/06.Project Merge File.txt", "r")
+
 recordCountInput = 0
 recordCountMerge = 0
+recordCountOutput = 0
 
 
 
-line = inputFile.readline()
-while line != '':
-    if(line == "**Insert Merge File Here**\n"):
-        for i in mergeFile:
-        line = mergeFile.readline()
-        print(line)
-        recordCountMerge += 1
+for line in inputFile:
+    if line.strip() == "**Insert Merge File Here**":
+        for mergeLine in mergeFile:
+            outputFile.write(mergeLine)
+            recordCountMerge += 1
+            recordCountOutput += 1
     else:
-        print(line)
+        outputFile.write(line)
         recordCountInput += 1
-    line = inputFile.readline()
+        recordCountOutput += 1
 
 mergeFile.close()
 inputFile.close()
 outputFile.close()
 
+print(f"{recordCountInput} input file records")
+print(f"{recordCountMerge} merge file records")
+print(f"{recordCountOutput} output file records")
